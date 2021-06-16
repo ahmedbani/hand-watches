0d19e5addafa8d5e7238545bf1544045f5dca161
AhmadBani2 <>
2021-06-16 13:43:51 +0000

    add the javaScript file

---
+++ hand-watch.js
@@ -0,0 +1,30 @@
+var name = prompt("Enter your name please ")
+confirm("Hi "+ name +"This your name correct! " );
+var x=prompt("whats the type of watches you are looking for ?")
+if(x = "Analog watch"){
+  document.write("nice choice you can check out the analog watches we have.")
+}
+else if(x = "Digital watch"){
+  document.write("nice choice you can check out the digital watches we have.")}
+else if(x = "Automatic watch"){
+  document.write("nice choice you can check out the automatic watches we have.")}
+else if(x = "Chronograph watch" ){
+  document.write("nice choice you can check out the chronograph watches we have.")}
+else if(x = "Diving watch"){
+  document.write("nice choice you can check out the diving watches we have.")}
+else if(x = "Dress watch"){
+  document.write("nice choice you can check out the dress watches we have.")}
+else if(x = "Quartz watch"){
+  document.write("nice choice you can check out the quartz watches we have.")}
+else if(x = "Mechanical watch"){
+  document.write("nice choice you can check out the mechanical watches we have.")}
+else if(x = "Pilot watch"){
+  document.write("nice choice you can check out the pilot watches we have.")}
+else if(x = "Field watch"){
+  document.write("nice choice you can check out the field watches we have.")}
+else if(x = "Smart watch"){
+  document.write("nice choice you can check out the smart watches we have.")}
+else if(x = "Luxary watch"){
+  document.write("nice choice you can check out the luxary watches we have.")}
+else {
+  document.write("Sorry we don't have this type of watches .")}
